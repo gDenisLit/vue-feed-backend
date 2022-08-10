e523c33817a2fec05be86067912e590f308a3cbb
Denis Lit <denislitviak@gmail.com>
2022-08-10 15:29:34 +0300

    remove unneccassery files and code

--- api/feed/feed.service.js
+++ api/feed/feed.service.js
@@ -1,6 +1,5 @@
 const dbService = require('../../services/db.service')
 const logger = require('../../services/logger.service')
-const ObjectId = require('mongodb').ObjectId
 const md5 = require('md5')
 
 async function query(filter) {
@@ -11,28 +10,6 @@ async function query(filter) {
         return feeds
     } catch (err) {
         logger.error('cannot find feeds', err)
-        throw err
-    }
-}
-
-async function getById(feedId) {
-    try {
-        const collection = await dbService.getCollection('feed')
-        const feed = collection.findOne({ _id: ObjectId(feedId) })
-        return feed
-    } catch (err) {
-        logger.error(`while finding feed ${feedId}`, err)
-        throw err
-    }
-}
-
-async function remove(feedId) {
-    try {
-        const collection = await dbService.getCollection('feed')
-        await collection.deleteOne({ _id: ObjectId(feedId) })
-        return feedId
-    } catch (err) {
-        logger.error(`cannot remove feed ${feedId}`, err)
         throw err
     }
 }
@@ -50,35 +27,19 @@ async function add(feed) {
         throw err
     }
 }
-async function update(feed) {
-    try {
-        var id = ObjectId(feed._id)
-        delete feed._id
-        const collection = await dbService.getCollection('feed')
-        await collection.updateOne({ _id: id }, { $set: { ...feed } })
-        console.log('updated feed', feed)
-        return feed
-    } catch (err) {
-        logger.error(`cannot update feed ${feedId}`, err)
-        throw err
-    }
-}
 
 module.exports = {
-    remove,
     query,
-    getById,
     add,
-    update,
 }
 
 function _buildCriteria(filterBy = {}) {
     const criteria = {}
     if (filterBy?.txt) {
         criteria['$or'] = [
-            {name: { $regex: filterBy.txt, $options: 'i' }},
-            {email: { $regex: filterBy.txt, $options: 'i' }},
-            {txt: { $regex: filterBy.txt, $options: 'i' }}
+            { name: { $regex: filterBy.txt, $options: 'i' } },
+            { email: { $regex: filterBy.txt, $options: 'i' } },
+            { txt: { $regex: filterBy.txt, $options: 'i' } }
         ]
     }
     return criteria
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
-
-// saasdasdasdasdasd
 const app = express()
 const http = require('http').createServer(app)
 
-// Express App Config
-app.use(cookieParser())
 app.use(express.json())
 // app.use(express.static('public'))
 
@@ -16,9 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve static files on production environment
   app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-  // Configuring CORS
   const corsOptions = {
-    // Make sure origin contains the url your frontend is running on
     origin: [
       'http://127.0.0.1:8080',
       'http://localhost:8080',
@@ -34,24 +27,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(cors(corsOptions))
 }
 
-const authRoutes = require('./api/auth/auth.routes')
-const userRoutes = require('./api/user/user.routes')
 const feedRoutes = require('./api/feed/feed.routes')
-const { setupSocketAPI } = require('./services/socket.service')
-
-// routes
-// routes
-// const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
-// app.all('*', setupAsyncLocalStorage)
-
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes)
 app.use('/api/feed', feedRoutes)
-setupSocketAPI(http)
 
-// Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/feed/123 it will still respond with
-// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
 app.get('/**', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
